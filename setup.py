5202448ce31f72e3644ced846174208f5e45a2ff
tinnawong <tin59001@gmail.com>
2023-09-19 18:36:51 +0700

    pushed to Pypi which is not the version

--- setup.py
+++ setup.py
@@ -2,29 +2,26 @@ import setuptools
 import subprocess
 import os
 
-cf_remote_version = (
+remote_version = (
     subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
     .stdout.decode("utf-8")
     .strip()
 )
 
-if "-" in cf_remote_version:
-    # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
-    # pip has gotten strict with version numbers
-    # so change it to: "1.3.3+22.git.gdf81228"
-    # See: https://peps.python.org/pep-0440/#local-version-segments
-    v,i,s = cf_remote_version.split("-")
-    cf_remote_version = v + "+" + i + ".git." + s
+if not remote_version.startswith("v"):
+    if "-" in remote_version:
+        v,i,s = remote_version.split("-")
+        remote_version = v + "+" + i + ".git." + s
 
-assert "-" not in cf_remote_version
-assert "." in cf_remote_version
+    assert "-" not in remote_version
+    assert "." in remote_version
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
     name="py_utility",
-    version=cf_remote_version,
+    version=remote_version,
     author="Tinnawong saelao",
     author_email="tinnawong2010@hotmail.com",
     description="python utility for research",
